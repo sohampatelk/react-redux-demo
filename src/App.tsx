57a6e92266eae66c5175a3ee03e5ee8a8f1b30d6
sohampatelk <sohampatelk@yahoo.com>
2020-04-16 11:59:46 -0600

    base for redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import { Grid, GridRow, Button } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid centered>
+      <Grid.Row>
+        <h1>Lets try React &amp; Redux..Again!</h1>
+      </Grid.Row>
+    </Grid>
   );
 }
-
 export default App;
